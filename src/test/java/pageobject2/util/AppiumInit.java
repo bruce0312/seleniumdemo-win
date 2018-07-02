8ac12051a6fe05ef18098712f8d81492d3ac27c0
bruce <bruceshen312@gmail.com>
2018-07-02 19:14:11 +0800

    finish appiuminit

--- src/test/java/pageobject2/util/AppiumInit.java
+++ src/test/java/pageobject2/util/AppiumInit.java
@@ -1,5 +1,8 @@
 package pageobject2.util;
 
 public class AppiumInit {
-
+    String platformName = "Android";
+    String deviceName = "xxx";
+    String appPackageName = "com.xueqiu.android";
+    String appActivity = ".view.WelcomeActivityAlias";
 }
